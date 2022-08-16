1354e7ae23bd4230fb99f1da3af8a76b168fe71d
an-saemi <ryogm97@gmail.com>
2022-08-16 18:18:40 +0900

    0816 update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 var swiper = new Swiper('.window', {
     direction: 'vertical',
     mousewheelControl: true,
@@ -6,10 +7,19 @@ var swiper = new Swiper('.window', {
     centeredSlides: true,
     autoplay: {
         delay: 2500,
+        pauseOnMouseEnter: true,
         disableOnInteraction: false,
     },
     loop: true,
-    resistance : false,
+    resistance: false,
     // freeMode: true,
     // freeModeSticky: true
 });
+
+const text = document.querySelector('.window');
+text.addEventListener('mouseover', function () {
+})
+
+document.addEventListener('scroll', function () {
+
+})
---
+++ js/skills.js
@@ -0,0 +1,12 @@
+$.ajax({
+    url: './json/skill.json',
+    success: function () {
+        console.log('fin');
+
+
+
+
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+    }
+})
