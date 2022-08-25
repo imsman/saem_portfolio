3a2096a1489a786e698dd43817c9c2fc822de0ec
an-saemi <106298277+an-saemi@users.noreply.github.com>
2022-08-25 18:40:58 +0900

    0825
    정리버전 낼 프로젝트 swiper물어보기

--- js/projects.js
+++ js/projects.js
@@ -30,30 +30,87 @@ $.ajax({
                     
                 </div>
             </li>`;
-
         });
-
-        // console.log($('.more a').val()); 비어있음 백그라운드 색회색으로 할까?+
-
         $('.content > ul').html(site);
 
-        $('.onClick').on('click',function(){
+
+        $('.onClick').on('click', function () {
             event.preventDefault();
             console.log($(this).attr('href'));
 
 
             let ind = $('div.more').index($(this).parent());
             console.log(ind);
-            
-            if(ind==2){
-                window.open(`${$(this).attr('href')}`,"_blank","width=380px,height=700px");
-            }else if(ind==1){
-            window.open(`${$(this).attr('href')}`,"_blank","width=680px,height=900px");
-            }else(
-                window.open(`${$(this).attr('href')}`,"_blank")
+
+            if (ind == 2) {
+                window.open(`${$(this).attr('href')}`, "_blank", "width=380px,height=700px");
+            } else if (ind == 1) {
+                window.open(`${$(this).attr('href')}`, "_blank", "width=680px,height=900px");
+            } else (
+                window.open(`${$(this).attr('href')}`, "_blank")
             )
         })
         //바로빌과 생활쏙법은 크기 달라야함
+
+
+        let artwork = '';
+        $.each(data.photoshop, function (k, v) {
+            artwork += `
+            <li class="swiper-slide"> 
+            <div class="slide">
+                <div class="intro">
+                    <p class="num">${v.num}</p>
+                    <p class="name">${v.name}</p>
+                    <p class="tool">${v.sub}></p>
+                    <p class="ex">${v.ex}</p>
+                </div>
+
+                <div class="pre">
+                    <img src="${v.img}" alt="">
+                </div>
+            </div>
+        </li>`;
+        })
+
+
+        let illust = '';
+        $.each(data.illust, function (k, v) {
+            illust += `
+            <li class="swiper-slide"> 
+            <div class="slide">
+                <div class="intro">
+                    <p class="num">${v.num}</p>
+                    <p class="name">${v.name}</p>
+                    <p class="tool">${v.sub}></p>
+                    <p class="ex">${v.ex}</p>
+                </div>
+
+                <div class="pre">
+                    <img src="${v.img}" alt="">
+                </div>
+            </div>
+        </li>`;
+        })
+
+
+        let video = '';
+        $.each(data.aftereffect, function (k, v) {
+            video += `
+            <li class="swiper-slide">
+                    <div class="slide">
+                        <p class="num">${v.num}</p>
+                            <p class="name">${v.name}</p>
+                            <p class="tool">${v.sub}></p>
+                            <p class="ex">${v.ex}</p>
+                        </div>
+
+                        <div class="pre">
+                            <iframe src="${v.url}">
+                        </div>
+                    </div>
+                    </div>
+                </li>`;
+        })
 
 
         var swiper = new Swiper(".mySwiper", {
@@ -76,5 +133,21 @@ $.ajax({
             //     crossFade: true
             // },
         });
+
+        let field = '';
+        $('.tab ul li').on('click', function () {
+            field = $(this).text();
+            console.log(field)
+
+            if (field == 'Web_projects') {
+                $('.content > ul').html(site);
+            } else if (field == 'Artwork') {
+                $('.content > ul').html(artwork);
+            } else if (field == 'Character') {
+                $('.content > ul').html(illust);
+            } else if (field == 'Video') {
+                $('.content > ul').html(video);
+            }
+        })
     }
 })
