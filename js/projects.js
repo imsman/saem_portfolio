92398ae4885b89827020369f3d968653526ea7ca
an-saemi <106298277+an-saemi@users.noreply.github.com>
2023-03-17 18:07:52 +0900

    projects 수정

--- js/projects.js
+++ js/projects.js
@@ -42,55 +42,56 @@ $.ajax({
             let ind = $('div.more').index($(this).parent());
             console.log(ind);
 
-            if (ind == 2) {
-                window.open(`${$(this).attr('href')}`, "_blank", "width=380px,height=700px");
-            } else if (ind == 1) {
+            if (ind == 1) {
                 window.open(`${$(this).attr('href')}`, "_blank", "width=680px,height=900px");
-            } else (
+            } 
+            else if (ind == 2) {
+                window.open(`${$(this).attr('href')}`, "_blank", "width=380px,height=700px");
+            } else {
                 window.open(`${$(this).attr('href')}`, "_blank")
-            )
+        }
         })
         //바로빌과 생활쏙법은 크기 달라야함
 
 
-        let artwork = '';
-        $.each(data.photoshop, function (k, v) {
-            artwork += `
-            <li class="swiper-slide"> 
-            <div class="slide">
-                <div class="intro">
-                    <p class="num">${v.num}</p>
-                    <p class="name">${v.name}</p>
-                    <p class="tool">${v.sub}></p>
-                    <p class="ex">${v.ex}</p>
-                </div>
+        // let artwork = '';
+        // $.each(data.photoshop, function (k, v) {
+        //     artwork += `
+        //     <li class="swiper-slide"> 
+        //     <div class="slide">
+        //         <div class="intro">
+        //             <p class="num">${v.num}</p>
+        //             <p class="name">${v.name}</p>
+        //             <p class="tool">${v.sub}></p>
+        //             <p class="ex">${v.ex}</p>
+        //         </div>
 
-                <div class="pre">
-                    <img src="${v.img}" alt="">
-                </div>
-            </div>
-        </li>`;
-        })
+        //         <div class="pre">
+        //             <img src="${v.img}" alt="">
+        //         </div>
+        //     </div>
+        // </li>`;
+        // })
 
 
-        let illust = '';
-        $.each(data.illust, function (k, v) {
-            illust += `
-            <li class="swiper-slide"> 
-            <div class="slide">
-                <div class="intro">
-                    <p class="num">${v.num}</p>
-                    <p class="name">${v.name}</p>
-                    <p class="tool">${v.sub}></p>
-                    <p class="ex">${v.ex}</p>
-                </div>
+        // let illust = '';
+        // $.each(data.illust, function (k, v) {
+        //     illust += `
+        //     <li class="swiper-slide"> 
+        //     <div class="slide">
+        //         <div class="intro">
+        //             <p class="num">${v.num}</p>
+        //             <p class="name">${v.name}</p>
+        //             <p class="tool">${v.sub}></p>
+        //             <p class="ex">${v.ex}</p>
+        //         </div>
 
-                <div class="pre">
-                    <img src="${v.img}" alt="">
-                </div>
-            </div>
-        </li>`;
-        })
+        //         <div class="pre">
+        //             <img src="${v.img}" alt="">
+        //         </div>
+        //     </div>
+        // </li>`;
+        // })
 
 
         let video = '';
